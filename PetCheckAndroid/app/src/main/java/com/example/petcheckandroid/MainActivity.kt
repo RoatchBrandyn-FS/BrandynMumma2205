f64faf684173d63e9f091aff6ad5fbb0c4629267
BrandynM <brandyn.roatch@hotmail.com>
2022-05-12 18:03:26 -0700

    Inital project with waka setup

---
+++ PetCheckAndroid/app/src/main/java/com/example/petcheckandroid/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.petcheckandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //I' am going to test that waka is keeping track of time for the project
+
+    }
+}
