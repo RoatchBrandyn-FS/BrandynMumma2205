dc743a1bfa15d72c6017769fe6fb882763059058
BrandynM <brandyn.roatch@hotmail.com>
2022-05-13 13:23:35 -0700

    added btns, set up validations for edit texts

--- PetCheckAndroid/app/src/main/java/com/example/petcheckandroid/MainActivity.java
+++ PetCheckAndroid/app/src/main/java/com/example/petcheckandroid/MainActivity.java
@@ -2,13 +2,66 @@ package com.example.petcheckandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.petcheckandroid.utilities.Alert;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private final String TAG = "MainActivity.TAG";
+
+    //elements
+    Button loginBtn;
+    TextView newRoomTV;
+    EditText roomCodeET;
+    EditText usernameET;
+    EditText passwordET;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-    
 
+        setButtons();
+    }
+
+    private void setButtons(){
+        //find btn and clickable textView
+        loginBtn = findViewById(R.id.login_btn_login);
+        newRoomTV = findViewById(R.id.login_textview_new_room);
+
+        loginBtn.setOnClickListener(this);
+        newRoomTV.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+
+        if(view.getId() == loginBtn.getId()){
+            Log.i(TAG, "onClick: Login Btn Pressed");
+
+            //set edittext
+            roomCodeET = findViewById(R.id.login_edit_room_code);
+            usernameET = findViewById(R.id.login_edit_username);
+            passwordET = findViewById(R.id.login_edit_password);
+
+            //convert to string
+            String roomCodeString = roomCodeET.getText().toString().trim();
+            String usernameString = usernameET.getText().toString().trim();
+            String passwordString = passwordET.getText().toString().trim();
+
+            if(roomCodeString.isEmpty() || usernameString.isEmpty() || passwordString.isEmpty()){
+                Alert.loginError(this);
+            }
+
+        }
+        else if (view.getId() == newRoomTV.getId()) {
+            Log.i(TAG, "onClick: New Room TV Pressed");
+        }
+    }
 }
---
+++ PetCheckAndroid/app/src/main/java/com/example/petcheckandroid/utilities/Alert.java
@@ -0,0 +1,26 @@
+package com.example.petcheckandroid.utilities;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+
+public class Alert {
+
+    public static void loginError(Context context){
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setTitle("Field(s) Empty");
+        builder.setMessage("Please fill in all needed information to login to your room.");
+        builder.setCancelable(false);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+
+        builder.show();
+
+    }
+
+}
